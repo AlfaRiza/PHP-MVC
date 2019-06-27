980ac58507bddd5d28e4c1870ae600a4e1113fc6
AlfaRiza <malfariza45@gmail.com>
2019-06-27 14:23:41 +0700

    memperbaiki tampilan dengan bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,8 @@
-
-    <h1>About index</h1>
+<div class="container">
+    <h1 class="mt-4">About index</h1>
     <p>Halo <?= $data['nama']; ?>, <?= $data['pekerjaan'] ?>, <?=$data['umur']?> tahun </p>
+
+    <img src="<?= BASEURL; ?>/img/1.JPG" 
+    alt="M.Alfa" width="200" class="rounded-circle shadow">
+
+</div>
