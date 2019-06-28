a8daaac001686454b3eec27d48ea221c0b43aab4
AlfaRiza <malfariza45@gmail.com>
2019-06-28 23:13:59 +0700

    menambahkan flsh message, delete data, update(dengan ajax) , dan searching

---
+++ public/js/script.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    $('.tombolTambahData').on('click', function () {
+        $('#formModalLabel').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+    });
+
+
+    $('.tampilModalUbah').on('click', function () {
+        $('#formModalLabel').html('ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal-body form').attr('action', 'http://localhost/phpmvc/public/mahasiswa/ubah');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost/phpmvc/public/mahasiswa/getUbah',
+            data: {
+                id: id
+            },
+            method: 'post',
+            dataType: 'json',
+            success: function (data) {
+                $('#nama').val(data.nama);
+                $('#nim').val(data.nim);
+                $('#jurusan').val(data.jurusan);
+                $('#id').val(data.id);
+            }
+        })
+
+
+    });
+});
